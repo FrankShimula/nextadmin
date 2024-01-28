4a584d5c3ae2a62f34d0736d447ee6f8409a6f41
Frank Shimula <josephmuller53@gmail.com>
2024-01-28 20:25:30 +0300

    menuitems

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,66 @@
+import styles from "./sidebar.module.css";
+import {
+  MdDashboard,
+  MdPerson,
+  MdShoppingCart,
+  MdCategory,
+  MdOutlineSettings,
+  MdHelpCenter,
+} from "react-icons/md";
+
+const menuItems = [
+  {
+    title: "Pages",
+    list: [
+      {
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: <MdDashboard />,
+      },
+      {
+        title: "Users",
+        path: "/dashboard/users",
+        icon: <MdPerson />,
+      },
+      {
+        title: "Products",
+        path: "/dashboard/products",
+        icon: <MdShoppingCart />,
+      },
+      {
+        title: "Categories",
+        path: "/dashboard/categories",
+        icon: <MdCategory />,
+      },
+    ],
+  },
+  {
+    title: "Users",
+    list: [
+      {
+        title: "Settings",
+        path: "/dashboard/settings",
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: "Help",
+        path: "/dashboard/help",
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className={styles.container}>
+      <ul>
+        {menuItems.map((item) => (
+          <li key={item.title}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
